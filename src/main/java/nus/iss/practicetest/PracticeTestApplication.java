6dd79d25b9a2cd84828dc679894c5745636f2236
vivientxy <vivientxy@hotmail.com>
2024-03-16 18:55:33 +0800

    init

---
+++ src/main/java/nus/iss/practicetest/PracticeTestApplication.java
@@ -0,0 +1,13 @@
+package nus.iss.practicetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nus/iss/practicetest/PracticeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.practicetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
